7180194f0cfbcf0b7ee334d6740ce010db55262e
JAIME RUIZ <jaimeruizxdd2@gmail.com>
2022-06-06 19:25:46 -0500

    upate
    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,11 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+//$app->bind('path.public',function(){
+//    return realpath('./../public/');
+//});
+
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
