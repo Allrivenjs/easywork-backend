0076eac3bc4966a599a00811e94c3de191870311
JAIME RUIZ <jaimeruizxdd2@gmail.com>
2022-07-26 18:50:40 -0500

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
             'birthday' => $this->faker->date(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'profile_photo_path' => 'Images/users/'.$this->faker->image('public/storage/Images/users', 640, 480, 'null', false),
+            'profile_photo_path' => $this->faker->imageUrl(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
