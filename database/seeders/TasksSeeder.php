4a8161da5af1d3485f9db5ae9ef2aae9a43b908c
JAIME RUIZ <jaimeruizxdd2@gmail.com>
2022-07-25 20:59:53 -0500

    Update TasksSeeder.php

--- database/seeders/TasksSeeder.php
+++ database/seeders/TasksSeeder.php
@@ -14,7 +14,7 @@ class TasksSeeder extends Seeder
      */
     public function run()
     {
-        task::factory()->count(100)->create()
+        task::factory()->count(10000)->create()
             ->each(fn ($task) => $task->topics()->attach([rand(1, 5), rand(6, 11)]));
     }
 }
